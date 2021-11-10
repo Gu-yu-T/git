da0eec028db91152a0b88f5dea030dc465ae3eec
txy <txy201712@163.com>
2021-11-10 21:22:56 +0800

    first commit

---
+++ src/main/java/com/jiangnan/GitTest.java
@@ -0,0 +1,12 @@
+package com.jiangnan;
+
+/**
+ * @author txy
+ * @Description
+ * @create 2021-11-10 21:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
