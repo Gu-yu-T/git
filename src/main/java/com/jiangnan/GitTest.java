10c8d9aecf62d279dbbf23c2de8fd8f56047faea
txy <txy201712@163.com>
2021-11-10 21:26:50 +0800

    second commit

--- src/main/java/com/jiangnan/GitTest.java
+++ src/main/java/com/jiangnan/GitTest.java
@@ -8,5 +8,6 @@ package com.jiangnan;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
